fdf20fc98be53e09dbcb9d0613e6236794cabaab
Brady Liechty <berrybloxinator@gmail.com>
2019-09-07 21:25:08 -0600

    Add dropdown for responsive content

--- js/index.js
+++ js/index.js
@@ -1,68 +1,17 @@
-// function placeholder() {
-//     function responsive(event) {
-//         if (event.matches) {
-//             document.getElementById('navigation').innerHTML =
-//                 `<div class='logo'>
-//                     <a href='index.html'><img src='../images/rockeyburgers02-logo.png' alt='rockey-logo' class='rockey-logo' height='40'/></a>
-//                 </div>
-//                 <i class="fas fa-bars bars" id='icon'></i>
-//                 <div id='drop-down-content'>
-//                     <div class='nav'>
-//                         <a href='menu.html'>Menu</a>
-//                         <a href='locations.html'>Locations</a>
-//                         <a href='jobs.html'>Jobs</a>
-//                         <a href='about.html'>About</a>
-//                         <a href='subscribe.html'>Subscribe</a>
-//                         <a href='faq.html'>FAQ</a>
-//                     </div>
-//                     <div class='socials'>
-//                         <a href='http://www.facebook.com/keyburgers' target='_blank'><i class='fab fa-twitter-square twitter'></i></a>
-//                         <a href='http://www.twitter.com/keyburgers' target='_blank'><i class='fab fa-facebook-square facebook'></i></a>
-//                     </div>
-//                 </div>`;
-//
-//             function display() {
-//                 let object = document.getElementById('drop-down-content');
-//                 if(object.style.display == 'none') {
-//                     object.style.display = 'block';
-//                 } else {
-//                     object.style.display = 'none';
-//                 }
-//             }
-//
-//             document.getElementById('icon').addEventListener('click', () => {
-//                 display();
-//             });
-//
-//             display();
-//         } else {
-//             document.getElementById('navigation').innerHTML =
-//                 `<div class='logo'>
-//                     <a href='index.html'><img src='../images/rockeyburgers02-logo.png' alt='rockey-logo' class='rockey-logo' height='40'/></a>
-//                 </div>
-//                 <div class='nav'>
-//                     <a href='menu.html'>Menu</a>
-//                     <a href='locations.html'>Locations</a>
-//                     <a href='jobs.html'>Jobs</a>
-//                     <a href='about.html'>About</a>
-//                     <a href='subscribe.html'>Subscribe</a>
-//                     <a href='faq.html'>FAQ</a>
-//                 </div>
-//                 <div class='socials'>
-//                     <a href='http://www.facebook.com/keyburgers' target='_blank'><i class='fab fa-twitter-square twitter'></i></a>
-//                     <a href='http://www.twitter.com/keyburgers' target='_blank'><i class='fab fa-facebook-square facebook'></i></a>
-//                 </div>`;
-//         }
-//     }
-//
-//     let mql = window.matchMedia('(max-width: 1200px)');
-//
-//     mql.addEventListener('change', (event) => {
-//         responsive(event);
-//     });
-//
-//     responsive(mql);
-// }
+function dropdown() {
+    let e = document.getElementById('drop-down-content').classList;
 
-function myFunction() {
+    e.remove('display-none');
+    e.add('display-flex');
+
+    let f = document.getElementById('resp-nav').style.display = 'none';
+}
+
+function closeNav() {
+    let e = document.getElementById('drop-down-content').classList;
+
+    e.remove('display-flex');
+    e.add('display-none');
+
+    let f = document.getElementById('resp-nav').style.display = 'flex';
 }
