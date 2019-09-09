ff06ef9f5ae75689d640de94ed449da01ac4390d
Brady Liechty <berrybloxinator@gmail.com>
2019-09-08 20:17:37 -0600

    Transform dropdown to side bar navigation

--- js/index.js
+++ js/index.js
@@ -1,23 +1,13 @@
-function dropdown() {
+function openNav() {
     let e = document.getElementById('drop-down-content').classList;
 
-    console.log(e[0]);
+    e.remove('display-none');
+    e.add('display-flex');
+}
 
-    let inList = false;
+function closeNav() {
+    let e = document.getElementById('drop-down-content').classList;
 
-    for (let className of e) {
-        console.log(className);
-        if (className === 'display-none') {
-            inList = true;
-            break;
-        }
-    }
-
-    if (inList === true) {
-        e.remove('display-none');
-        e.add('display-flex');
-    } else {
-        e.remove('display-flex');
-        e.add('display-none');
-    }
+    e.remove('display-flex');
+    e.add('display-none');
 }
